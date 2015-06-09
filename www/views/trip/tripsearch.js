5bc345204d167af2afb213e3cfddb48b3fc3db13
Dimple Yuen <dimpleyuen@gmail.com>
2015-06-09 12:46:46 +0800

    take order modal and call function

--- www/views/trip/tripsearch.js
+++ www/views/trip/tripsearch.js
@@ -1,33 +1,55 @@
 angular.module('App')
 
-.controller('TripsearchCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
+.controller('TripsearchCtrl', ['$scope', '$http', '$location', '$ionicModal', function($scope, $http, $location, $ionicModal){
 
   var url = "http://localhost:3000/"
 
 
-
+// Get All Trips
   $scope.getOpenTrips = function(status) {
     $http.get(url + '/posts?status=' + status).success(function(response){
       console.log(response);
       $scope.posts = response;
-    
-
     });
   }
 
   $scope.getOpenTrips('open');
 
-  $scope.updateStatusToProcessing = function(index) {
-    $scope.posts[index].status.content = "processing"
-    
-    var put_data = {
+// Ionic Modal Setup
+  $ionicModal.fromTemplateUrl('templates/modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  
+// Update Status To Processing
+  $scope.updateStatusToProcessing = function(index) {    
+    var patch_processing_data = {
       post: {
-        status: $scope.posts[index].status.content
+        status: "processing"
       }
     }
 
-    $http.put(url + 'posts/' + $scope.posts[index].id, put_data).success(function(response) {
+    $http.patch(url + 'posts/' + $scope.posts[index].id, patch_processing_data).success(function(response) {
+      console.log(response);
+      $scope.modal.show();
+    })
+  }
+
+// Update Status Back To Open
+  $scope.cancelOrder = function(index) {
+    $scope.modal.hide();
+    
+    var patch_open_data = {
+      post: {
+        status: "open"
+      }
+    }
+
+    $http.patch(url + 'posts/' + $scope.posts[index].id, patch_open_data).success(function(response) {
       console.log(response);
     })
+
   }
 }]);
